b2842a0187bb444d2484a58ed5271d199689330c
HanLinXin <hanlinxin316@sohu.com>
2017-01-18 17:05:18 +0800

    jianfang v1.6

---
+++ js/base.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Hanlinxin on 2017/1/18.
+ */
+$(function () {
+    if ($(this).scrollTop() >= 160){
+        $("#nav").addClass("affix");
+    }
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 160){
+            $("#nav").addClass("affix");
+        }else {
+            $("#nav").removeClass("affix");
+        };
+    });
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+$('body').scrollspy({
+    target: '.navbar-example',
+    offset: 70
+});
+
+$('.navbar-example a').click(function () {
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 55
+    }, 500);
+    return false;
+});
